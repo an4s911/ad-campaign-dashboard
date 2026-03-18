c48193e89d3a71ad763a54a110ee70ce8efea4fb
Anas Bashir <nasbas23@gmail.com>
2026-03-18 17:38:09 +0300

    fix preview image error

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -28,7 +28,7 @@ export async function POST(request: NextRequest) {
       });
 
       return NextResponse.json(
-        { url: blob.url },
+        { url: blob.downloadUrl },
         { status: 201 }
       );
     } else {
